f7b8bcf79a0b59e241b88a582414db2a8e82f958
Ivet Ivanova <ivet.ivanova@ibm.com>
2018-08-01 10:31:02 +0300

    buildScripts

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
 
 
- var express = require('express');
- var path = require('path');
- var open = require('open');
+ import express from 'express';
+ import path from 'path';
+ import open from 'open';
 
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', function (req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'))
@@ -16,6 +16,6 @@ app.listen (port , function(err){
     if(err){
         console.log(err);
     } else {
-        open('http://localhost:'+port)
+        open('http://localhost:'+port);
     }
-})
+});
